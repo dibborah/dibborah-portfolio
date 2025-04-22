eb11a0a50b536a7c8dfd8496d2fb65de589fe53a
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-22 23:11:37 +0530

    deploy setup done

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import NotFound from "./pages/NotFound";
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Index />} />
+      <Route path="/dibborah-portfolio" element={<Index />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
